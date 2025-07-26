e3e30a0b793f1a88613d4f567b3deecbe45c1868
abood.jarrar <abd.a.jarrar@gmail.com>
2025-07-26 12:21:39 +0300

    finalyze handling the hold button

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 let activePlayer = 0;
 let curScore = 0;
-let score1 = 0;
-let score2 = 0;
+let scores = [0, 0];
+let isPlaying = true;
 // selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
@@ -11,6 +11,7 @@ const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnRollEl = document.querySelector('.btn--roll');
+const btnHoldEl = document.querySelector('.btn--hold');
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -35,7 +36,24 @@ function changePlayer() {
 }
 
 btnRollEl.addEventListener('click', function () {
-  const dice = Math.floor(Math.random() * 6) + 1;
-  disPic(dice);
-  dice !== 1 ? addScore(dice) : changePlayer();
+  if (isPlaying) {
+    const dice = Math.floor(Math.random() * 6) + 1;
+    disPic(dice);
+    dice !== 1 ? addScore(dice) : changePlayer();
+  }
+});
+btnHoldEl.addEventListener('click', function () {
+  if (isPlaying) {
+    scores[activePlayer] += curScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 10) {
+      isPlaying = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      diceEl.classList.add('hidden');
+    } else changePlayer();
+  }
 });
